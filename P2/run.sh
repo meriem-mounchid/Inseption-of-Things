f3c61f7d9fc395085c269617c270b67ad134ef34
meriem-mounchid <meriemmounchid1@gmail.com>
2021-12-28 00:59:36 +0100

    add p2

---
+++ P2/run.sh
@@ -0,0 +1,8 @@
+
+echo "before apply"
+/usr/local/bin/kubectl apply -f app1.yaml
+/usr/local/bin/kubectl apply -f app2.yaml
+/usr/local/bin/kubectl apply -f app3.yaml
+/usr/local/bin/kubectl apply -f service.yaml
+/usr/local/bin/kubectl apply -f ingress.yaml
+echo "after apply"
--- P3/script.sh
+++ P3/script.sh
@@ -17,8 +17,9 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 
 ### install K3D ###
 curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+sudo k3d cluster create my-cluster --port 8080:80@loadbalancer --port 8443:443@loadbalancer --port 8888:81@loadbalancer --k3s-arg "--disable=traefik@server:0" 
 #test: k3d version
-# sudo k3d cluster delete demo-cluster
+#test: kubectl cluster-info
 
 
 ### Argo CD CLI ###
@@ -26,33 +27,7 @@ sudo curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/rele
 sudo chmod +x /usr/local/bin/argocd
 
 
-#sudo k3d cluster create my-cluster --port 8080:443@loadbalancer --k3s-arg "--disable=traefik@server:0"
-sudo k3d cluster create my-cluster --port 8080:80@loadbalancer --port 8443:443@loadbalancer --port 8888:81@loadbalancer --k3s-arg "--disable=traefik@server:0" 
-
-#test: kubectl cluster-info
-
 ### ArgoCD ###
-
 sudo kubectl create namespace argocd
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 sudo kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-
-#LOG IN
-sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
-argocd login <ARGOCD_SERVER>
-#
-
-# sudo kubectl delete ns dev
-
-sudo kubectl apply -f application.yaml
-# sudo kubectl get ns
-# sudo kubectl get pods -n dev
-# sudo kubectl get svc -A
-# sudo kubectl get ep -A
-# sudo kubectl get deploy -A
-# sudo kubectl get rs -A
-
-
-
-# sudo docker pull wil42/playground:v1
-# sudo docker image history wil42/playground:v1
