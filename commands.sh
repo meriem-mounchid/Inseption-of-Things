28a7511eded76b06180c107369aee84782d99ad7
M I S Λ K I <43518207+meriem-mounchid@users.noreply.github.com>
2022-01-02 12:46:23 +0100

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ### LOG IN (ArgoCD) ### 
-sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
-argocd login <ARGOCD_SERVER>
+# sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+# argocd login <ARGOCD_SERVER>
 
 # sudo kubectl delete ns dev
 # sudo kubectl get ns
@@ -12,5 +12,9 @@ argocd login <ARGOCD_SERVER>
 # sudo kubectl get deploy -A
 # sudo kubectl get rs -A
 
+### Move ur file on ur host to ur vm ###
+# scp script.sh application.yaml misaki@10.12.100.165:~
+
+### See wil42 port ###
 # sudo docker pull wil42/playground:v1
 # sudo docker image history wil42/playground:v1
